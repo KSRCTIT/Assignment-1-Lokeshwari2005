44107e3aaf93ec65041793d5653fc907eb3311bd
Lokeshwari C <lokeshwari0825@gmail.com>
2025-08-10 11:19:54 +0530

    Eligiblevote.cs

---
+++ Eligiblevote.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int age = 18;
+
+        if (age >= 18)
+        {
+            Console.WriteLine("Eligible to Vote");
+        }
+        else
+        {
+            Console.WriteLine("Not Eligible");
+        }
+    }
+}
