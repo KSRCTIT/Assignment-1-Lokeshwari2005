0a4cf7f43ccb2576d3006fb56c05bd182b6b0bc6
Lokeshwari2005 <lokeshwari0825@gmail.com>
2025-08-10 11:48:36 +0530

    success Login.cs

---
+++ Login.cs
@@ -0,0 +1,13 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        bool loginSuccess = true;
+
+        if (loginSuccess)
+            Console.WriteLine("Login successful");
+        else
+            Console.WriteLine("Access denied");
+    }
+}
